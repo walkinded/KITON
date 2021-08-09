c65c24c393d49d8a73995a0acdffac17dc6cbcf0
Walkinded <labunskij1997@gmail.com>
2021-08-09 12:44:39 +0300

    added libs done main-block

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('.header__button-menu').click(function() {
+    $('body').addClass('overflow-hidden');
+    $('.header__mobile').fadeIn();
+  });
+
+  $('.header__mobile-close').click(function() {
+    $('body').removeClass('overflow-hidden');
+    $('.header__mobile').fadeOut();
+  });
+});
