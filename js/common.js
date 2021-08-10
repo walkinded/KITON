66c6e7d5db495fab96ec5a9e5acbf9f618312630
Walkinded <labunskij1997@gmail.com>
2021-08-10 17:28:28 +0300

    added animations + done popular-product-groups block

--- js/common.js
+++ js/common.js
@@ -19,3 +19,73 @@ $(window).scroll(function() {
       $('.menu').removeClass('menu_scrolled');  
     }
 }); 
+
+
+$(document).ready(function($) {
+  $('.tab_content').hide();
+  $('.tab_content:first').show();
+  $('.tabs li:first').addClass('active');
+  $('.tabs li').click(function(event) {
+    $('.tab_content').slick('refresh');
+    $('.tabs li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab_content').hide();
+
+    var selectTab = $(this).find('a').attr("href");
+
+    $(selectTab).fadeIn();
+  });
+});
+
+$('.tab_content').slick({
+  arrows: true,
+  slidesToShow: 4,
+  rows: 1,
+  speed: 1000,
+  // autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1601,
+      settings: {
+        arrows: true,
+        slidesToShow: 3,
+        // speed: 1000,
+      }
+    },
+    {
+      breakpoint: 1280,
+      settings: {
+        arrows: true,
+        slidesToShow: 2,
+        // speed: 1000,
+      }
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+        // speed: 1000,
+        arrows: true,
+      }
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        // speed: 1000,
+        arrows: false,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 641,
+      settings: {
+        slidesToShow: 1,
+        // speed: 1000,
+        arrows: false,
+        dots: true,
+      }
+    }
+    
+  ]
+});
